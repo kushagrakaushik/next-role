b8abf2a9c831e3343f6d2ce4a74a5f6bffcea7c5
Mani Amar Sehgal <sehgalmaniamar@gmail.com>
2026-05-04 23:18:12 +0530

    Changed Landing Page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import { Link } from 'react-router-dom'
 
 export default function Landing() {
   return (
     <div>
       <Navbar />
-      <div className='w-full h-screen flex items-center justify-center'>
-        <h1 className='text-4xl font-bold'>Next Role</h1>
+      <div className='w-full h-screen flex flex-col gap-10 items-center justify-center'>
+        <h1 className='text-4xl'>Next Role</h1>
+        <h2 className='text-7xl'>blahh..</h2>
+        <Link to="/signup">
+        <button className='text-2xl bg-purple-600 p-3 rounded-xl'>Get Started</button>
+        </Link>
       </div>
     </div>
   )
