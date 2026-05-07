35309a9002be751901d50307913664f1caeab664
Kushagra Kaushik <kushagra.k25239@nst.rishihood.edu.in>
2026-05-07 23:23:59 +0530

    fixed no roles bug due to merge

--- src/App.jsx
+++ src/App.jsx
@@ -43,12 +43,10 @@ function App() {
           <Route path="/" element={<Landing user={user} />} />
           <Route path="/signup" element={<Signup user={user} />} />
           <Route path="/login" element={<Login user={user} />} />
-          <Route path="/dashboard" element={<Dashboard user={user} />} />
-          <Route path="/roles" element={<Roles />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/profile" element={<Profile />} />
           <Route path="/dashboard" element={<Dashboard user={user} groupedJobs={groupedJobs} jobsLoading={jobsLoading} />} />
           <Route path="/roles" element={<Roles groupedJobs={groupedJobs} jobsLoading={jobsLoading} />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </main>
     </div>
